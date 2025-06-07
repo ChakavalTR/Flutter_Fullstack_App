57029e7110c11fe68aeadd3f643a606976d698c9
TR Val <ChakavalTR@yahoo.com>
2025-06-07 16:47:35 +0700

    more practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_fullstack_app/practice_views_1/home_page.dart';
+import 'package:flutter_fullstack_app/practice_view_2/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeView());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
